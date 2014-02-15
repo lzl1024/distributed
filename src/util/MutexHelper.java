97f51919bb6d5278de3bcc0ee42923aad654dd11
lzl1024 <zhuolinliu8@gmail.com>
2014-02-14 22:12:13 -0500

    basic logic done

--- src/util/MutexHelper.java
+++ src/util/MutexHelper.java
@@ -1,9 +1,11 @@
 package util;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import message.MessagePasser;
 import message.MulticastMessage;
 import util.Config.CS_STATUS;
 
@@ -20,10 +22,12 @@ public class MutexHelper {
     }
     
     // CS information
-    public static CS_STATUS csStatus;
-    public static ConcurrentLinkedQueue<MulticastMessage> voteQueue;
-    public static boolean voted;
-    public static HashSet<String> getVoted;
+    private static CS_STATUS csStatus;
+    private static ConcurrentLinkedQueue<MulticastMessage> voteQueue;
+    private static boolean voted;
+    private static HashSet<String> getVoted;
+    private static int numofGrpMember;
+    private static MessagePasser passer;
     
     public static void init(){
         // initiate CS information
@@ -31,24 +35,51 @@ public class MutexHelper {
         voteQueue = new ConcurrentLinkedQueue<MulticastMessage>();
         voted = false;
         getVoted = new HashSet<String>();
+        passer = MessagePasser.getInstance();
+        numofGrpMember = passer.groupInfo.get(passer.localName).size();
     }
 
     /**
      * Handle message according to its type
      * @param message
+     * @throws IOException 
      */
-    public static void handleMutexMsg(MulticastMessage message) {
+    public static void handleMutexMsg(MulticastMessage message) throws IOException {
         MSG_TYPE type = MSG_TYPE.valueOf(message.getKind());
-        //MessagePasser passer = MessagePasser.getInstance();
-        
+          
         switch(type) {
         case REQUEST:
-            break;
-        case RELEASE:
+            if (csStatus == CS_STATUS.IN_CS || true == voted) {
+                voteQueue.offer(message);
+            } else {
+                // send back vote message
+                voted = true;
+                MulticastMessage msg = new MulticastMessage(null, "VOTE", "vote");
+                msg.setDest(message.get_source());
+                msg.set_source(passer.localName);
+                passer.send(msg, false);
+            }
             break;
         case VOTE:
-        }
-        
-    }
-    
+            getVoted.add(message.get_source());
+            // if get all of the vote, get into the CS
+            if (getVoted.size() == numofGrpMember) {
+                System.out.println("INFO: " + passer.localName + " get into the critical section!");
+                csStatus = CS_STATUS.IN_CS;
+            }
+        case RELEASE:
+            if (voteQueue.isEmpty()) {
+                voted = false;
+            } else {
+                // vote the head of the queue
+                voted = true;
+                String dest = voteQueue.poll().get_source();
+                MulticastMessage msg = new MulticastMessage(null, "VOTE", "vote");
+                msg.setDest(dest);
+                msg.set_source(passer.localName);
+                passer.send(msg, false);
+            }
+            break;
+        }  
+    }  
 }
